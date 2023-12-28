ae0a9e748e8aa4a4322e4f1bf59aba4daef0c3eb
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2023-12-27 17:01:56 -0800

    Responsive design

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -3,9 +3,9 @@ import Navbar from '@/components/Navbar';
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen bg-[#eeeeee] font-lexend">
+    <main className="min-h-screen w-screen bg-[#eeeeee] font-lexend">
       <Navbar />
-      <div className="flex flex-col items-center justify-center my-10 sm:my-20 gap-8 sm:gap-12">
+      <div className="flex flex-col items-center justify-center my-10 sm:my-20 gap-8 sm:gap-12 px-4">
         <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6">
           <Image 
             src="/logo.png"
@@ -15,8 +15,8 @@ export default function Home() {
             layout="responsive"
           />
         </div>
-        <h1 className="text-lg sm:text-lg lg:text-4xl font-bold">Hintersight Studios</h1>
-        <div className="text-sm sm:text-md lg:text-3xl text-center">
+        <h1 className="text-2xl sm:text-lg lg:text-4xl font-bold text-center">Hintersight Studios</h1>
+        <div className="text-center text-xs sm:text-md lg:text-3xl">
           <div>Be one of the first people to hear about <span className="font-bold">Project "AUTO-COMBAT"</span></div>
           <div>our upcoming hybrid board game based on the auto-battler genre!</div>
         </div>
--- client/components/Navbar.jsx
+++ client/components/Navbar.jsx
@@ -1,38 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
-import Image from 'next/image'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Navbar = () => {
   return (
-    <div className="h-[200px] w-full flex items-center p-12 px-32 justify-between">
-      <div className="">
+    <nav className="w-full flex items-center justify-between p-4 md:p-12 lg:px-32">
+      <div>
         <Link href="/">
-          <Image 
-            src="/logo.png"
-            width={50}
-            height={50}
-            alt="Hinterlight Studios Logo"
-          />
+          
+            <Image 
+              src="/logo.png"
+              width={50}
+              height={50}
+              alt="Hinterlight Studios Logo"
+              layout="fixed"
+            />
+          
         </Link>
       </div>
 
-      <div className="sm:space-x-4 lg:space-x-12 font-bold sm:text-lg lg:text-xl">
-        <Link href="/">
-          Home
-        </Link>
-        <Link href="/about">
-          About
-        </Link>
-        <Link href="/about">
-          Blog
-        </Link>
-        <Link href="/about">
-          Contact
-        </Link>
-
+      <div className="md:flex space-x-4 lg:space-x-8 font-bold text-sm md:text-lg lg:text-xl">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/blog">Blog</Link> {/* Assuming '/blog' is the correct path */}
+        <Link href="/contact">Contact</Link> {/* Assuming '/contact' is the correct path */}
       </div>
-    </div>
-  )
-}
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
