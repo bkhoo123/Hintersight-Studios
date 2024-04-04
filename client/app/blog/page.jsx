c38b128411025eaca7fb79cec550d6e3bfc72a39
Brian Khoo <102838003+bkhoo123@users.noreply.github.com>
2024-04-04 13:30:28 -0700

    Fixed Wrong Alt Image String

--- client/app/blog/page.jsx
+++ client/app/blog/page.jsx
@@ -8,7 +8,7 @@ const page = () => {
       <Navbar />
          <div className="flex flex-col items-center justify-center my-10 sm:my-20 gap-8 sm:gap-12 px-4">
          <div className="w-1/2 sm:w-1/3 md:w-1/4 lg:w-1/6">
-            <Image src="/logo.png" width={200} height={200} alt="Hinterlight Studios Logo" layout="responsive" />
+            <Image src="/logo.png" width={200} height={200} alt="Hintersight Studios Logo" layout="responsive" />
          </div>
          <h1 className="text-2xl sm:text-lg lg:text-4xl font-bold text-center">Hintersight Studios</h1>
          <div className="text-center text-xs sm:text-md lg:text-3xl">
